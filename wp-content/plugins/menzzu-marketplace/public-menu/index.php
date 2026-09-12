17115e1e08650f3934f429d85b814eb39c64ff42
wallacenana <walacenatanrodrigues@gmail.com>
2026-09-12 17:53:20 -0300

    Republish reverted address flow

--- wp-content/plugins/menzzu-marketplace/public-menu/index.php
+++ wp-content/plugins/menzzu-marketplace/public-menu/index.php
@@ -1225,7 +1225,7 @@ try {
         </script>
 
         <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11" defer></script>
-                <script type="text/javascript" src="<?php echo esc_url(MENZZU_MARKETPLACE_URL . 'public-menu/script.js?v=2.15'); ?>" defer></script>
+                <script type="text/javascript" src="<?php echo esc_url(MENZZU_MARKETPLACE_URL . 'public-menu/script.js?v=2.18'); ?>" defer></script>
 
     </html>
 <?php
