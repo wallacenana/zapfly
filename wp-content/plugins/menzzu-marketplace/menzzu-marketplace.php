8bd784f957c70a37c2d74e7a00bfff8f66bfc504
wallacenana <walacenatanrodrigues@gmail.com>
2026-09-11 21:05:50 -0300

    Disable browser address autofill in checkout

--- wp-content/plugins/menzzu-marketplace/menzzu-marketplace.php
+++ wp-content/plugins/menzzu-marketplace/menzzu-marketplace.php
@@ -3,7 +3,7 @@
 /**
  * Plugin Name: Menzzu Marketplace
  * Description: Marketplace Menzzu com descoberta de lojas, busca e catalogo.
- * Version: 3.1.30
+ * Version: 3.1.31
  * Author: Menzzu
  */
 
