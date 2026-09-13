88b007ef25d17d1c9d4ef303d6f6ba17d21fe250
wallacenana <walacenatanrodrigues@gmail.com>
2026-09-13 10:57:57 -0300

    Bump plugin version for multi-image fields

--- wp-content/plugins/menzzu-marketplace/menzzu-marketplace.php
+++ wp-content/plugins/menzzu-marketplace/menzzu-marketplace.php
@@ -3,7 +3,7 @@
 /**
  * Plugin Name: Menzzu Marketplace
  * Description: Marketplace Menzzu com descoberta de lojas, busca e catalogo.
- * Version: 3.1.90
+ * Version: 3.1.91
  * Author: Menzzu
  */
 
