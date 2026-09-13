e7e7dd59cf296597cb762e9978308ecc722d4c0e
wallacenana <walacenatanrodrigues@gmail.com>
2026-09-13 18:16:51 -0300

    Revert "Align plugin header version"
    This reverts commit 086d3c0c2027b681b594bbb7d2b00017d88d0d39.

--- wp-content/plugins/menzzu-marketplace/menzzu-marketplace.php
+++ wp-content/plugins/menzzu-marketplace/menzzu-marketplace.php
@@ -3,7 +3,7 @@
 /**
  * Plugin Name: Menzzu Marketplace
  * Description: Marketplace Menzzu com descoberta de lojas, busca e catalogo.
- * Version: 3.1.93
+ * Version: 3.1.92
  * Author: Menzzu
  */
 
