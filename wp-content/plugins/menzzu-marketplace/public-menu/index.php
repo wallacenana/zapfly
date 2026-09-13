9543db848e7e6eeea833fd1da4164bccedb71039
wallacenana <walacenatanrodrigues@gmail.com>
2026-09-12 23:45:35 -0300

    Adjust item detail modal layout

--- wp-content/plugins/menzzu-marketplace/public-menu/index.php
+++ wp-content/plugins/menzzu-marketplace/public-menu/index.php
@@ -395,7 +395,7 @@ try {
         <script>
             window.__SSR__ = <?php echo json_encode($ssrData, JSON_HEX_TAG | JSON_HEX_AMP); ?>;
         </script>
-        <link rel="stylesheet" href="<?php echo esc_url(MENZZU_MARKETPLACE_URL . 'public-menu/style.css?v=4.04'); ?>">
+        <link rel="stylesheet" href="<?php echo esc_url(MENZZU_MARKETPLACE_URL . 'public-menu/style.css?v=4.05'); ?>">
         <style>
             :root {
                 --primary-color:
