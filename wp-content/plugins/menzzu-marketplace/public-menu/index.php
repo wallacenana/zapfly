41e376d49433a368bb8357d3d827acbf6f08e47e
wallacenana <walacenatanrodrigues@gmail.com>
2026-09-14 10:10:58 -0300

    Bump public menu script version

--- wp-content/plugins/menzzu-marketplace/public-menu/index.php
+++ wp-content/plugins/menzzu-marketplace/public-menu/index.php
@@ -1297,7 +1297,7 @@ try {
         </script>
 
         <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11" defer></script>
-                <script type="text/javascript" src="<?php echo esc_url(MENZZU_MARKETPLACE_URL . 'public-menu/script.js?v=2.51'); ?>" defer></script>
+                <script type="text/javascript" src="<?php echo esc_url(MENZZU_MARKETPLACE_URL . 'public-menu/script.js?v=2.52'); ?>" defer></script>
 
     </html>
 <?php
